448eab9b9b6a7c0e25c688ce33c6ef0cef1dce3c
James <james.hallam93@gmail.com>
2021-03-24 16:32:33 +0000

    fixed postreducer tests

--- family_app/src/reducers/tests/postReducer.test.js
+++ family_app/src/reducers/tests/postReducer.test.js
@@ -1,3 +1,4 @@
+import { create } from "json-server"
 import postActions from "../../actions/postActions"
 const { newPostAction, updateLikesAction, initPostsAction } = postActions
 import postReducer from "../postReducer"
@@ -13,7 +14,7 @@ describe("new post action creator ", ()=>{
 
         const expectedAction = {
             type: "NEW_POST",
-            data: newPost
+            payload: newPost
         }
         const createdAction = newPostAction(newPost)
         expect(createdAction).toEqual(expectedAction)
@@ -24,7 +25,7 @@ describe("update likes action creator ", ()=>{
     test("creates expected action", ()=>{
         const expectedAction = {
             type: "UPDATE_LIKES",
-            data: newPost
+            payload: newPost
         }
         const createdAction = updateLikesAction(newPost)
         expect(createdAction).toEqual(expectedAction)
@@ -32,25 +33,29 @@ describe("update likes action creator ", ()=>{
 })
 
 describe("the post reducer ", ()=>{
-    test("should return an empty array on startup given no action type or data", ()=>{
-        expect(postReducer(undefined, {})).toEqual([])
+    test("should return an empty object on startup given no action type or payload", ()=>{
+        expect(postReducer(undefined, {})).toEqual({})
     })
     describe("in the case of a new post ", ()=>{
         test("adds the post to the store successfully", ()=>{
             const createPostAction = newPostAction(newPost)
-            const returnedPost = postReducer([],createPostAction)[0]
-            expect(returnedPost).toEqual(newPost)
+
+            const returnedPost = postReducer({posts:[]},createPostAction)
+
+            expect(returnedPost.posts[0]).toEqual(newPost)
         })
     })
     describe("in the case of updating a post", ()=>{
         test("the returned posts likes are updated successfully", ()=>{
+
             const updatedPost = {
                 ...newPost,
                 likes:newPost.likes + 1
             }
             const updatePostAction = updateLikesAction(updatedPost)
-            const returnedPost = postReducer([newPost], updatePostAction)[0]
-            expect(returnedPost.likes).toEqual(newPost.likes + 1)
+            const returnedPost = postReducer({posts:[newPost]}, updatePostAction)
+            
+            expect(returnedPost.posts[0].likes).toEqual(newPost.likes + 1)
         })
     })
 
